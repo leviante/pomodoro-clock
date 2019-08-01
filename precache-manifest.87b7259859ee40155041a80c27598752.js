ed37325b9a88e352f6e2f7128fdfd08fbbc3d399
Berk Açıkgöz <acikgozberk96@gmail.com>
2019-08-01 22:30:18 +0300

    Updates

---
+++ precache-manifest.87b7259859ee40155041a80c27598752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ac9ff3ccfc847b13d0d0efec6fd303",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "eb67ab8d7753522d0b97",
+    "url": "/pomodoro-clock/static/css/main.8fc67365.chunk.css"
+  },
+  {
+    "revision": "77bd24ff47482f1540af",
+    "url": "/pomodoro-clock/static/js/2.f6730175.chunk.js"
+  },
+  {
+    "revision": "eb67ab8d7753522d0b97",
+    "url": "/pomodoro-clock/static/js/main.d3e2e8f2.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  }
+]);
